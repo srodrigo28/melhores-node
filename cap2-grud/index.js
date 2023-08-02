a69decc5629bfa28aad33c18daef8496a5316263
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-02 07:29:04 -0300

    aula 9 cap2 finalizada com restify funcionando

--- cap2-grud/index.js
+++ cap2-grud/index.js
@@ -1,14 +1,19 @@
-const http = require('http')
+var restify = require('restify')
 
-const hostname = 'localhost'
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-type', 'text/plain')
-    res.end('Hello World2\n')
+const server = restify.createServer({
+    name: 'myapp',
+    version: '1.0.0'
 })
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+server.use(restify.plugins.acceptParser(server.acceptable))
+server.use(restify.plugins.queryParser())
+server.use(restify.plugins.bodyParser())
+
+server.get('/echo/:nome', function (req, res, next) {
+    res.send(req.params)
+    return next()
+})
+
+server.listen(3000, function () {
+    console.log('%s listening at %s', server.name, server.url)
 })
